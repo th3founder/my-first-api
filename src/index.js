67298dea3685ca8b1dfdb4cd5da6d624af51ead3
Alvaro <th3founder>
2022-06-27 20:08:29 -0500

    My first page with and API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,71 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+//url
+const baseUrl = "https://platzi-avo.vercel.app";
+const url = "https://platzi-avo.vercel.app/api/avo";
 
-console.log('Happy hacking :)')
+const appNode = document.querySelector('#app')
+
+/* const formatPrice = (price) =>{
+    const newPrice = new window.Intl.NumberFormat("en-EN",{
+        style:"curreyncy",
+        currency: "USD",
+    }).format(price)
+
+    return newPrice
+} */
+
+//intl
+//1 - formato a fecha
+//2 - formato a monedas
+
+//web api
+const getData = async (url_api) =>{
+    try{
+        const respuesta = await window.fetch(`${url_api}/api/avo`) //esta es para la info
+        const imagenesURL = await window.fetch(`${url_api}`)//esta es para las imagenes porque necesita ir sin /api/avo
+        const data = await respuesta.json()
+        const blockElements = []
+        data.data.forEach(element => {
+            
+
+            //crear imagen
+            const img = document.createElement('img');
+            img.src = `${imagenesURL.url}${element.image}`//mandamos a llamar imagenes
+            //crear titulo
+
+
+            const title = document.createElement('h2')
+            title.textContent = element.name
+            /* title.className = "text-2xl text-red-600" */
+
+            //crear precio
+            const price = document.createElement('div')
+            /* price.textContent = formatPrice(element.price) */
+            price.textContent = `$${element.price}`
+
+            //creamos un contenor que almacenara cada card
+            const container = document.createElement('div')
+            container.className = "card"
+            /* container.appendChild(img)
+            container.appendChild(title)
+            container.appendChild(price) */
+            container.append(img,title,price)//nos permite agregar a todos en orden
+            //usar este:
+            blockElements.push(container)
+
+            //este no es el mejor:
+            /* 
+                document.body.appendChild(container)
+            */
+            console.log(img,title,price)
+
+            
+        });
+
+        appNode.append(...blockElements)
+
+    }catch(e){
+        console.log(e)
+    }
+}
+
+getData(baseUrl)
